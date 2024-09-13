657178191dee1c0fcf3222d7814ea089f325b12d
Ionut Hodoroaga <ionut.hodoroaga20@gmail.com>
2024-09-13 18:07:16 +0300

    spring project setup and csv data functional

---
+++ src/main/java/com/electricvehicles/electric_vehicles_app/ElectricVehiclesAppApplication.java
@@ -0,0 +1,13 @@
+package com.electricvehicles.electric_vehicles_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricVehiclesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricVehiclesAppApplication.class, args);
+	}
+
+}
